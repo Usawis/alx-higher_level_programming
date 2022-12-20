fc57316745e28e7e3b7c8c2e468026ce2bdcba66
Wisdom Usa <triplespy793@gmail.com>
2022-12-20 22:55:23 +0100

    a class Square that defines a square by: (based on 6-square.py)

--- 0x06-python-classes/101-square.py
+++ 0x06-python-classes/101-square.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define a class Square."""
 
 
@@ -8,7 +7,6 @@ class Square:
 
     def __init__(self, size=0, position=(0, 0)):
         """Initialize a new square.
-
         Args:
             size (int): The size of the new square.
             position (int, int): The position of the new square.
@@ -69,4 +67,3 @@ class Square:
             if i != self.__size - 1:
                 print("")
         return ("")
-
