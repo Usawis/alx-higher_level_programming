2bb2c1d3f78494fa002a1c0f03bdeacf7cc36dda
Wisdom Usa <triplespy793@gmail.com>
2022-12-20 17:58:19 +0100

    update task 2

--- 0x06-python-classes/2-square.py
+++ 0x06-python-classes/2-square.py
@@ -1,4 +1,5 @@
 #!/usr/bin/python3
+
 """Define a class Square."""
 
 
@@ -7,6 +8,7 @@ class Square:
 
     def __init__(self, size=0):
         """Initialize a new Square.
+
         Args:
             size (int): The size of the new square.
         """
@@ -14,4 +16,5 @@ class Square:
             raise TypeError("size must be an integer")
         elif size < 0:
             raise ValueError("size must be >= 0")
-        self.__size = 
+        self.__size = size
+
