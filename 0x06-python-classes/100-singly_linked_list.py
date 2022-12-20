0836bd1e51f40e47561cbdbed81c43a5de7247a3
Wisdom Usa <triplespy793@gmail.com>
2022-12-20 22:53:27 +0100

    a class Node that defines a node of a singly linked list by

--- 0x06-python-classes/100-singly_linked_list.py
+++ 0x06-python-classes/100-singly_linked_list.py
@@ -1,5 +1,4 @@
 #!/usr/bin/python3
-
 """Define classes for a singly-linked list."""
 
 
@@ -8,7 +7,6 @@ class Node:
 
     def __init__(self, data, next_node=None):
         """Initialize a new Node.
-
         Args:
             data (int): The data of the new Node.
             next_node (Node): The next node of the new Node.
@@ -43,15 +41,13 @@ class SinglyLinkedList:
     """Represent a singly-linked list."""
 
     def __init__(self):
-        """Initalize a new SinglyLinkedList."""
+        """Initialize a new SinglyLinkedList."""
         self.__head = None
 
     def sorted_insert(self, value):
         """Insert a new Node to the SinglyLinkedList.
-
         The node is inserted into the list at the correct
         ordered numerical position.
-
         Args:
             value (Node): The new Node to insert.
         """
@@ -78,4 +74,3 @@ class SinglyLinkedList:
             values.append(str(tmp.data))
             tmp = tmp.next_node
         return ('\n'.join(values))
-
